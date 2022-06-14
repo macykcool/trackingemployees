8232cac0ebe19c7756d87d467f649dc65144f0d5
Macy Kay Mannix <macymannix@gmail.com>
2022-06-13 19:39:15 -0700

    testing

---
+++ db/connection.js
@@ -0,0 +1,5 @@
+//queries will be in a class and i export them for index depending on what user wants to do
+
+//routes refernce connection
+
+//index fetches routes , passes the data to be used in the queries 
